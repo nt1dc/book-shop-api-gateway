a6bb8afee00679ef35f60615141b40ab9817b7b5
nt1dc <nt1dcare@gmail.com>
2023-03-22 02:07:24 +0300

    swagger added creation orders and payments works

--- src/main/kotlin/se/nt1dc/apigateway/ApiGatewayApplication.kt
+++ src/main/kotlin/se/nt1dc/apigateway/ApiGatewayApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class ApiGatewayApplication
-
 fun main(args: Array<String>) {
     runApplication<ApiGatewayApplication>(*args)
 }
